e3acb12d44af4ec13f97e74040e8b82321a32b0f
flyfilly <fnsanchez317@gmail.com>
2024-05-20 14:00:54 -0500

    pinsa theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@
 import { useState, useEffect } from "react";
 
 import DonutChart from "./components/donut-chart";
-import dynamic from "next/dynamic";
-
-const Slides = dynamic(() => import("@/app/components/slides"), { ssr: false });
+import styles from "./main.module.css";
 
 export default function Devices() {
   const [percent, setPercent] = useState(0);
@@ -18,22 +16,8 @@ export default function Devices() {
   }, []);
 
   return (
-    <Slides title="Demo Dashboard">
-      <section className="h-full w-full">
-        <div className="h-full w-full flex justify-center items-center">
-          <DonutChart precentComplete={percent} className="h-1/2" />
-        </div>
-      </section>
-      {/* <section className="h-full w-full">
-        <section className="h-full w-full flex justify-center items-center">
-          Slide 1.2
-        </section>
-      </section> */}
-      <section className="h-full w-full">
-        <div className="h-full w-full grid items-center">
-          <h1 className="text-4xl font-bold text-center">Slide 2</h1>
-        </div>
-      </section>
-    </Slides>
+    <main className={`h-screen w-screen ${styles.main}`}>
+      <DonutChart precentComplete={percent} className="h-1/2" />
+    </main>
   );
 }
