12415564119f09dac7fb20ff67326c42f3eb12dd
flyfilly <fnsanchez317@gmail.com>
2024-05-21 15:59:49 -0500

    added more detail to the widget

--- app/components/donut-chart/index.tsx
+++ app/components/donut-chart/index.tsx
@@ -1,20 +1,23 @@
 "use client";
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState, useEffect, act } from "react";
 
 import styles from "./donutChart.module.css";
 
 export type DonutChartProps = {
   actual: number;
   expected: number;
+  manufacturingOrder: string;
 
   className?: string;
   style?: React.CSSProperties;
 };
 
 const DonutChart: React.FC<DonutChartProps> = ({
-  style = {},
-  expected,
   actual,
+  expected,
+  manufacturingOrder = "MO123456",
+
+  style = {},
   className = "h-96 w-96",
 }) => {
   const [percent, setPercent] = useState(
@@ -46,6 +49,14 @@ const DonutChart: React.FC<DonutChartProps> = ({
       className={`${className} ${styles.donutChart}`}
     >
       <circle
+        cx="20.8"
+        cy="21"
+        r="20"
+        fill="transparent"
+        stroke="#FFFFFF"
+        strokeWidth=".1"
+      />
+      <circle
         cx="21"
         cy="21"
         r="15.9"
@@ -64,10 +75,10 @@ const DonutChart: React.FC<DonutChartProps> = ({
 
       <text
         className={`${styles.text} ${styles.textSmall} font-normal`}
-        x="16"
+        x="15.25"
         y="-26.5"
       >
-        MO12345
+        {manufacturingOrder}
       </text>
 
       <text
@@ -79,6 +90,27 @@ const DonutChart: React.FC<DonutChartProps> = ({
       </text>
       <text className={`${styles.text} ${styles.textSmall}`} x="15.25" y="-14">
         PROGRESS
+      </text>
+
+      <circle cx="35" cy="34.25" r="3" fill="var(--background-color)" />
+
+      <text className={`${styles.text} ${styles.textSmall}`} x="32.75" y="-35">
+        {expected}
+      </text>
+      <text className={`${styles.text} ${styles.smallLabel}`} x="33" y="-33.5">
+        Goal
+      </text>
+
+      <circle cx="8.25" cy="6.5" r="3" fill="var(--background-color)" />
+      <text className={`${styles.text} ${styles.textSmall}`} x="5.35" y="-7.95">
+        {actual}
+      </text>
+      <text
+        className={`${styles.text} ${styles.smallLabel}`}
+        x="4.75"
+        y="-6.35"
+      >
+        Done
       </text>
     </svg>
   );
