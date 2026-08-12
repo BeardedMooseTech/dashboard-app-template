6c480753096f0bd1b5f17757b321f9ba4df788c1
flyfilly <fnsanchez317@gmail.com>
2026-08-12 10:38:08 -0500

    refactor query structure for MrpProduction to improve readability

--- app/api/dashboards/route.ts
+++ app/api/dashboards/route.ts
@@ -24,13 +24,9 @@ export async function POST() {
             {
               query: `
               query {
-              MrpProduction(domain:[
-                  ['state', '=', 'progress'],
-                  ['|', '|',   
-                  ['product_id.default_code', 'ilike', 'WBF'],
-                  ['product_id.default_code', 'ilike', 'RBF'],
-                  ['product_id.default_code', 'ilike', 'TBF'],
-                ]) {
+              MrpProduction(
+                domain: [["state", "=", "progress"], "|", "|", ["product_id.default_code", "ilike", "WBF"], ["product_id.default_code", "ilike", "RBF"], ["product_id.default_code", "ilike", "TBF"]]
+              ) {
                   name
                   state
                   mo_name
